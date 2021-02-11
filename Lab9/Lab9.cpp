ceb541b930e574e370b6d66a4a3c5df4a1b73939
XYCKU <48044503+XYCKU@users.noreply.github.com>
2021-02-11 18:17:57 +0300

    Lab9 fixed

--- Lab9/Lab9.cpp
+++ Lab9/Lab9.cpp
@@ -1,19 +1,35 @@
 #include <cstdio>
+#include <iostream>
 
-double rec(int i)
-{ // Рекурсивная функция
-    if (i > 0)
-        return 1. / i + rec(i - 1);
+double rec(int i, int n, double recur) // Рекурсивная функция
+{ 
+    recur /= i; // Рекурент
+    if (i <= n)
+        return recur + rec(i + 1, n, recur);
     return 0;
+}
+
+double notRec(int n) // Не рекурсивная функция
+{ 
+    double ans = 1;
+    double recur = 1; // Рекурент
+    for (int i = 2; i <= n; i++)
+    {
+        recur /= i; // Вычисление рекурента
+        ans += recur; // Прибавление к ответу
+    }
+    return ans;
 }
 
 int main()
 {
+    setlocale(LC_ALL, "Russian"); // Поддержка кирилицы
     int k; // Переменная
-    printf("Vvedite K: "); // Ввод данных
+    printf("Введите K: "); // Ввод данных
     scanf_s("%d", &k);
 
-    printf("%.4lf\n", rec(k)); // Вывод ответа
+    printf("Результат при рекурентном подсчете: %10.5lf\n", rec(1, k, 1)); // Подсчет рекурентно
+    printf("Результат при не рекурентном подсчете: %7.5lf\n", notRec(k)); // Подсчет не рекурентно
 
     return 0;
 }
